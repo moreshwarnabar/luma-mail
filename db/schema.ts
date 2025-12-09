fee3d2ae713a9650103d6e67686de55a40b0911e
moreshwarnabar <mrnabar@gmail.com>
2025-12-08 19:50:52 -0700

    feat: add email and account schema with custom types for citext and enums for email labels and providers

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,18 @@
-import { boolean, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import {
+  boolean,
+  integer,
+  json,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+} from 'drizzle-orm/pg-core';
+import { primaryKey } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
 
+import { citext } from './customTypes';
+
+// BETTER-AUTH
 export const user = pgTable('user', {
   id: text('id').primaryKey(),
   name: text('name').notNull(),
@@ -61,3 +74,240 @@ export const verification = pgTable('verification', {
     .$onUpdate(() => /* @__PURE__ */ new Date())
     .notNull(),
 });
+
+/* ------- CORE TABLES ------- */
+
+export const providerEnum = pgEnum('provider', [
+  'gmail',
+  'outlook',
+  'imap',
+  'yahoo',
+]);
+
+export const emailLabelEnum = pgEnum('email_label', ['inbox', 'sent', 'draft']);
+
+export const mailAccount = pgTable('mail_account', {
+  id: text('id').primaryKey(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => user.id, { onDelete: 'cascade' }),
+
+  provider: providerEnum().notNull(),
+  providerAccountId: text('provider_account_id').notNull().unique(),
+  address: text('address').notNull().unique(),
+
+  accessToken: text('access_token').notNull(),
+  refreshToken: text('refresh_token'),
+  accessTokenExpiresAt: timestamp('access_token_expires_at'),
+  refreshTokenExpiresAt: timestamp('refresh_token_expires_at'),
+
+  lastSyncedAt: timestamp('last_synced_at')
+    .defaultNow()
+    .$onUpdate(() => /* @__PURE__ */ new Date())
+    .notNull(),
+});
+
+export const thread = pgTable('thread', {
+  id: text('id').primaryKey(),
+  mailAccountId: text('mail_account_id')
+    .notNull()
+    .references(() => mailAccount.id, { onDelete: 'cascade' }),
+
+  subject: text('subject'),
+  lastMessageDate: timestamp('last_message_date'),
+  participantIds: text('participant_ids').array(),
+
+  inboxStatus: boolean('inbox_status').default(true),
+  draftStatus: boolean('draft_status').default(false),
+  sentStatus: boolean('sent_status').default(false),
+});
+
+export const email = pgTable('email', {
+  id: text('id').primaryKey(),
+  threadId: text('thread_id').references(() => thread.id, {
+    onDelete: 'cascade',
+  }),
+
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at')
+    .defaultNow()
+    .$onUpdate(() => /* @__PURE__ */ new Date())
+    .notNull(),
+  sentAt: timestamp('sent_at').defaultNow().notNull(),
+  receivedAt: timestamp('received_at').defaultNow().notNull(),
+
+  subject: text('subject'),
+  fromId: text('from_id').references(() => emailAddress.id, {
+    onDelete: 'cascade',
+  }),
+
+  hasAttachments: boolean('has_attachments'),
+  body: text('body'),
+  bodySnippet: text('body_snippet'),
+
+  inReplyTo: text('in_reply_to'),
+  references: text('references'),
+
+  internetHeaders: json('internet_headers').array(),
+  nativeProperties: json('native_properties'),
+  folderId: text('folder_id'),
+  omitted: text('omitted'),
+
+  emailLabel: emailLabelEnum(),
+});
+
+export const emailAddress = pgTable('email_address', {
+  id: text('id').primaryKey(),
+  name: text('name'),
+  address: citext('address').notNull().unique(),
+  accountId: text('account_id')
+    .unique()
+    .references(() => mailAccount.id, {
+      onDelete: 'cascade',
+    }),
+});
+
+export const emailAttachments = pgTable('email_attachments', {
+  id: text('id').primaryKey(),
+  name: text('name'),
+  mimeType: text('mime_type').notNull(),
+  size: integer('size').notNull(),
+  inline: boolean('inline'),
+  contentId: text('content_id'),
+  content: text('content'),
+  contentLocation: text('content_location'),
+  emailId: text('email_id').references(() => email.id, { onDelete: 'cascade' }),
+});
+
+/* -------- JOIN TABLES ------- */
+
+export const emailToRecipients = pgTable(
+  'email_to_recipients',
+  {
+    emailId: text('email_id')
+      .notNull()
+      .references(() => email.id, { onDelete: 'cascade' }),
+    emailAddressId: text('email_address_id')
+      .notNull()
+      .references(() => emailAddress.id, { onDelete: 'cascade' }),
+  },
+  t => [primaryKey({ columns: [t.emailId, t.emailAddressId] })]
+);
+
+export const emailCcRecipients = pgTable(
+  'email_cc_recipients',
+  {
+    emailId: text('email_id')
+      .notNull()
+      .references(() => email.id, { onDelete: 'cascade' }),
+    emailAddressId: text('email_address_id')
+      .notNull()
+      .references(() => emailAddress.id, { onDelete: 'cascade' }),
+  },
+  t => [primaryKey({ columns: [t.emailId, t.emailAddressId] })]
+);
+
+export const emailBccRecipients = pgTable(
+  'email_bcc_recipients',
+  {
+    emailId: text('email_id')
+      .notNull()
+      .references(() => email.id, { onDelete: 'cascade' }),
+    emailAddressId: text('email_address_id')
+      .notNull()
+      .references(() => emailAddress.id, { onDelete: 'cascade' }),
+  },
+  t => [primaryKey({ columns: [t.emailId, t.emailAddressId] })]
+);
+
+export const emailReplyToRecipients = pgTable(
+  'email_reply_to_recipients',
+  {
+    emailId: text('email_id')
+      .notNull()
+      .references(() => email.id, { onDelete: 'cascade' }),
+    emailAddressId: text('email_address_id')
+      .notNull()
+      .references(() => emailAddress.id, { onDelete: 'cascade' }),
+  },
+  t => [primaryKey({ columns: [t.emailId, t.emailAddressId] })]
+);
+
+/* -------- RELATIONS ------- */
+
+export const emailRelations = relations(email, ({ one, many }) => ({
+  from: one(emailAddress, {
+    fields: [email.fromId],
+    references: [emailAddress.id],
+  }),
+
+  to: many(emailToRecipients),
+  cc: many(emailCcRecipients),
+  bcc: many(emailBccRecipients),
+  replyTo: many(emailReplyToRecipients),
+}));
+
+export const emailAdressRelations = relations(emailAddress, ({ many }) => ({
+  sentEmails: many(email),
+
+  receivedTo: many(emailToRecipients),
+  receivedCc: many(emailCcRecipients),
+  receivedBcc: many(emailBccRecipients),
+  replyToEmails: many(emailReplyToRecipients),
+}));
+
+export const emailToRecipientsRelations = relations(
+  emailToRecipients,
+  ({ one }) => ({
+    email: one(email, {
+      fields: [emailToRecipients.emailId],
+      references: [email.id],
+    }),
+    address: one(emailAddress, {
+      fields: [emailToRecipients.emailAddressId],
+      references: [emailAddress.id],
+    }),
+  })
+);
+
+export const emailCcRecipientsRelations = relations(
+  emailCcRecipients,
+  ({ one }) => ({
+    email: one(email, {
+      fields: [emailCcRecipients.emailId],
+      references: [email.id],
+    }),
+    address: one(emailAddress, {
+      fields: [emailCcRecipients.emailAddressId],
+      references: [emailAddress.id],
+    }),
+  })
+);
+
+export const emailBccRecipientsRelations = relations(
+  emailBccRecipients,
+  ({ one }) => ({
+    email: one(email, {
+      fields: [emailBccRecipients.emailId],
+      references: [email.id],
+    }),
+    address: one(emailAddress, {
+      fields: [emailBccRecipients.emailAddressId],
+      references: [emailAddress.id],
+    }),
+  })
+);
+
+export const emailReplyToRecipientsRelations = relations(
+  emailReplyToRecipients,
+  ({ one }) => ({
+    email: one(email, {
+      fields: [emailReplyToRecipients.emailId],
+      references: [email.id],
+    }),
+    address: one(emailAddress, {
+      fields: [emailReplyToRecipients.emailAddressId],
+      references: [emailAddress.id],
+    }),
+  })
+);
